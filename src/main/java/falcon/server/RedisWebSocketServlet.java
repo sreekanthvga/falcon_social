0308d593afe41bd5f87267a60da79bddbe6b71f8
sreekanthvga <sreekanthvga@users.noreply.github.com>
2015-11-22 20:55:39 +0100

    Update RedisWebSocketServlet.java

--- src/main/java/falcon/server/RedisWebSocketServlet.java
+++ src/main/java/falcon/server/RedisWebSocketServlet.java
@@ -5,13 +5,10 @@ import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 import redis.clients.jedis.JedisPool;
 
 public class RedisWebSocketServlet extends WebSocketServlet {
-	//private static final long serialVersionUID = -9152808383213487609L;
 	private JedisPool pool;
-	
 	public RedisWebSocketServlet(JedisPool pool) {
 		this.pool = pool;
 	}
-	
 	@Override
 	public void configure(WebSocketServletFactory factory) {
 		factory.setCreator(new WebSocketFactory(pool));
